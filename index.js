bb225960a62df8cc959ebc7382d92ea441afca48
ayse8888 <aysebasar@yahoo.com>
2023-04-01 23:49:35 +0300

    pagination added

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ $(function () {
             "Date": "Sales Representative",
             "Employee": "Sales Representative",
             "#": "Sales Representative",
-        },          
+        },
         ],
     });
 });
